1d3403d6c19b68dd7f81c15678413aee84bfcf7d
jiale zhou <z429213642@gmail.com>
2025-01-09 21:14:18 +0800

    feat:添加解题

---
+++ src/example/fibonacci.rs
@@ -0,0 +1,18 @@
+/// ```  
+/// # 启动命令
+/// cargo run --example fibonacci
+/// ```
+fn main() {
+    let n = 20;
+    println!("fib({n}) = {}", fib(n));
+}
+
+fn fib(n: u32) -> u32 {
+    if n <= 2 {
+        // The base case.
+        1
+    } else {
+        // The recursive case.
+        fib(n - 1) + fib(n - 2)
+    }
+}
