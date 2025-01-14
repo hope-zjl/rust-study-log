4df4584b3827e8535f4640ad81ca3f75f18057ab
jiale zhou <z429213642@gmail.com>
2025-01-14 21:07:51 +0800

    feat:跳出循环，跳过本次循环

---
+++ src/example/breakOrContinue.rs
@@ -0,0 +1,17 @@
+/// ```  
+/// # 启动命令
+/// cargo run --example cycle
+/// ```
+fn main() {
+    let mut i = 0;
+    loop {
+        i += 1;
+        if i > 5 {
+            break;
+        }
+        if i % 2 == 0 {
+            continue;
+        }
+        println!("{}", i);
+    }
+}
