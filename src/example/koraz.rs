39a59d8b8163f1bf15f1a018ef867a5108786689
jiale zhou <z429213642@gmail.com>
2025-01-15 21:54:19 +0800

    feat:添加考拉兹序列

---
+++ src/example/koraz.rs
@@ -0,0 +1,19 @@
+/// ```  
+/// # 启动命令
+/// cargo run --example koraz
+/// ```
+fn main() {
+    println!("len: {}", collatz_length(4564685));
+}
+
+fn collatz_length(mut n: i32) -> i32 {
+    let mut len = 1;
+    let mut vec = vec![];
+    while n > 1 {
+        n = if n % 2 == 0 { n / 2 } else {3 * n + 1};
+        len += 1;
+        vec.push(n);
+    }
+    println!("vec: {:?}", vec);
+    len
+}
