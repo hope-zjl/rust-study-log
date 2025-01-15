6a3c5b2919652e6efc0b6811092d85f22579bfa1
jiale zhou <z429213642@gmail.com>
2025-01-15 23:49:23 +0800

    style:格式化代码

--- src/example/koraz.rs
+++ src/example/koraz.rs
@@ -10,7 +10,7 @@ fn collatz_length(mut n: i32) -> i32 {
     let mut len = 1;
     let mut vec = vec![];
     while n > 1 {
-        n = if n % 2 == 0 { n / 2 } else {3 * n + 1};
+        n = if n % 2 == 0 { n / 2 } else { 3 * n + 1 };
         len += 1;
         vec.push(n);
     }
--- src/example/labels.rs
+++ src/example/labels.rs
@@ -7,7 +7,7 @@ fn main() {
     let mut elements_searched = 0;
     let target_value = 10;
     // 标签的命名 《 '标签名: 》 在这里调用标签可以直接跳出所指定的循环
-    'labe: for i in 0..=2  {
+    'labe: for i in 0..=2 {
         for j in 0..=2 {
             elements_searched += 1;
             if s[i][j] == target_value {
