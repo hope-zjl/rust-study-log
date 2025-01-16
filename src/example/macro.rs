4fc6afbd54f11962a62af1fcbb7fd8d3650ee6e9
jiale zhou <z429213642@gmail.com>
2025-01-16 21:15:49 +0800

    feat:宏

---
+++ src/example/macro.rs
@@ -0,0 +1,20 @@
+/// ```
+/// # 启动命令
+/// cargo run --example macro
+/// ```
+fn main() {
+    let n = 4;
+    println!("{n}! = {}", factorial(n));
+}
+
+fn factorial(n: u32) -> u32 {
+    let mut x = 1;
+    for i in 1..=n {
+        x *= dbg!(i);
+    }
+    x
+}
+
+fn _fizzbuzz() {
+    todo!("未被执行")
+}
