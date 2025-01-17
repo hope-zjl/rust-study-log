2c884fa8918c9aa2909502d8ee1fd5c42b97edd8
jiale zhou <z429213642@gmail.com>
2025-01-17 21:49:00 +0800

    feat:数组

---
+++ src/example/arr.rs
@@ -0,0 +1,10 @@
+/// ```
+/// # 启动命令
+/// cargo run --example arr
+/// ```
+fn main() {
+    let mut a: [i32; 10] = [19; 10];
+    a[4] = 2;
+    println!("a: {:?}", a);
+    println!("a[1]: {:?}", a[1]);
+}
