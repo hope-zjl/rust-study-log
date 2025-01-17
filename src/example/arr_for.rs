68ff69cb3ad67b4dc5ba945cea13d8f387bace4a
jiale zhou <z429213642@gmail.com>
2025-01-17 22:09:23 +0800

    feat:数组迭代循环

---
+++ src/example/arr_for.rs
@@ -0,0 +1,12 @@
+/// ```
+/// # 启动命令
+/// cargo run --example arr_for
+/// ```
+fn main() {
+    let arr = [7, 9, 22, 76, 15, 44, 3];
+    for s in arr {
+        for x in s..5 {
+            dbg!(x);
+        }
+    }
+}
