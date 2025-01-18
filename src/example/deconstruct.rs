c4f6fdd3181883644e7d2e18d1d1adef597b4022
jiale zhou <z429213642@gmail.com>
2025-01-18 20:25:04 +0800

    feat:元组结构

---
+++ src/example/deconstruct.rs
@@ -0,0 +1,19 @@
+/// ```
+/// # 启动命令
+/// cargo run --example deconstruct
+/// ```
+fn main() {
+    dup_deconstruct((15, 20));
+    deconstruct_dup((70, 80));
+}
+
+fn dup_deconstruct(tup: (i32, i32)) {
+    let left = tup.0;
+    let right = tup.1;
+    println!("left1: {left}, right1: {right}");
+}
+
+fn deconstruct_dup(tup: (i32, i32)) {
+    let (left, right) = tup;
+    println!("left2: {left}, right2: {right}");
+}
