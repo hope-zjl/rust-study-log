9c91b965b9937b312eb242c9cb73633be062095d
jiale zhou <z429213642@gmail.com>
2025-01-26 22:52:10 +0800

    docs:添加注释

--- src/example/method_traits/traits.rs
+++ src/example/method_traits/traits.rs
@@ -5,18 +5,22 @@
 /// ```
 
 trait Pet {
+    // 接口
     fn talk(&self) -> String;
     fn greet(&self) {
         println!("输出内容: {}", self.talk())
     }
 }
 
+// 结构体
 struct Dog {
     name: String,
     age: i8,
 }
 
+// 方法的实现
 impl Pet for Dog {
+    // 用来实现接口的具体行为
     fn talk(&self) -> String {
         format!("name: {} , age: {}", self.name, self.age)
     }
