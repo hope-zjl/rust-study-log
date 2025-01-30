c3f39ba77349e2de60d6b51d69633a3a1a014bdb
zjl <z429213642@gmail.com>
2025-01-30 20:26:29 +0800

    feat:添加倍数

--- src/example/method_traits/derived_traits.rs
+++ src/example/method_traits/derived_traits.rs
@@ -18,4 +18,22 @@ fn main() {
     p2.strength = 7;
     p2.hit_points = 9;
     println!("{:?}, {:?}", p1, p2);
+
+    multiple_n(7, 4);
+}
+
+fn multiple_n(n: usize, init: usize) {
+    for i in init..201 {
+        if i % n == 0 && str_cont(i, n) {
+            println!("过");
+        }
+    }
+}
+
+fn str_cont(x1: usize, x2: usize) -> bool {
+    if x1.to_string().contains(&x2.to_string()) {
+        true
+    } else {
+        false
+    }
 }
