97a871ce0429c818736d10ba8e2c2b61caf5b0e3
jiale zhou <z429213642@gmail.com>
2025-02-09 21:48:04 +0800

    feat:添加Impl trait 泛型

---
+++ src/example/method_traits/generics_impl_traits.rs
@@ -0,0 +1,22 @@
+/// ```
+/// # 启动命令
+///  cargo run --example generics_impl_traits
+///
+/// ```
+
+fn add_42_millions(x: impl Into<i32>) -> i32 {
+    x.into() + 42_000_000
+}
+
+fn pair_of(x: i32) -> impl std::fmt::Debug {
+    (x + 1, x - 1)
+}
+
+fn main() {
+    let many = add_42_millions(42_i8);
+    println!("{many}");
+    let many_more = add_42_millions(10_000_000);
+    println!("{many_more}");
+    let debuggable = pair_of(27);
+    println!("{debuggable:?}");
+}
